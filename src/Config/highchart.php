8227364adea5e8ecf9ff057e3ca13e2c39edc9f1
BBYC <boyz357159@gmail.com>
2019-04-08 19:01:23 +0800

    disable exporting_js

--- src/Config/highchart.php
+++ src/Config/highchart.php
@@ -3,6 +3,6 @@
 return [
     'highchart_js'      => true,
     'series_label_js'   => true,
-    'exporting_js'      => true,
+    'exporting_js'      => false,
     'export_data_js'    => false,
 ];
